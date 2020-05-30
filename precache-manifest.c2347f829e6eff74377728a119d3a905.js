354dffcbfa0a79e241b8e52ca2b0bea68b77321f
Abhinav Dubey <49772834+abhinavdubeyad9@users.noreply.github.com>
2020-05-30 21:05:04 +0530

    Updates

---
+++ precache-manifest.c2347f829e6eff74377728a119d3a905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158178be53fd012f43159267473dc9d3",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "96cb15f0680e3ce3d9f6",
+    "url": "/TodoList/static/css/main.c11cea5e.chunk.css"
+  },
+  {
+    "revision": "58633b453c889b2d347e",
+    "url": "/TodoList/static/js/2.adb3aca4.chunk.js"
+  },
+  {
+    "revision": "96cb15f0680e3ce3d9f6",
+    "url": "/TodoList/static/js/main.36d312c0.chunk.js"
+  },
+  {
+    "revision": "41ffe862c61462c98130",
+    "url": "/TodoList/static/js/runtime~main.1e3ae2d4.js"
+  }
+]);
